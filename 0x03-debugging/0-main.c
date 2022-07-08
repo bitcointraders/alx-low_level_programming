0290f0f09d5e91e2d9d3288d19d5ccc69b562725
bitcoinbuyers <ugudansam@yahoo.com>
2022-07-08 16:49:55 -0700

    Main-0,Main-H

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,23 +2,22 @@
 
 /**
 
- * main - tests function that prints if integer is positive or negative
+* main - tests function that prints if integer is positive or negative
 
- * Return: 0
+* Return: 0
 
- */
+*/
 
 int main(void)
-  
-{
-  
-  int i;
-    
-  i = 0;
-  
-  positive_or_negative(i);
-  
-  return (0);
-  
-}
 
+{
+
+int i;
+
+i = 0;
+
+positive_or_negative(i);
+
+return (0);
+
+}
